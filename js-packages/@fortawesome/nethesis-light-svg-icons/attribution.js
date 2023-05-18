7dfa96b81c667fc6855056631e07bdff4c8c572d
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2023-05-18 09:40:51 +0200

    icons set. light: added new icons

---
+++ js-packages/@fortawesome/nethesis-light-svg-icons/attribution.js
@@ -0,0 +1,5 @@
+var date = new Date().getFullYear();
+console.log(`Nethesis Icon Set by @fontawesome - https://fontawesome.com
+License - https://github.com/FortAwesome/Font-Awesome/blob/6.x/LICENSE.txt
+Copyright ${date} Nethesis srl.
+`)
---
+++ js-packages/@fortawesome/nethesis-light-svg-icons/index.d.ts
@@ -0,0 +1,6 @@
+export const faHeadset: IconDefinition;
+
+import { IconDefinition, IconLookup, IconName, IconPrefix, IconPack } from '@fortawesome/fontawesome-common-types';
+export { IconDefinition, IconLookup, IconName, IconPrefix, IconPack } from '@fortawesome/fontawesome-common-types';
+export const prefix: IconPrefix;
+export const fas: IconPack;
