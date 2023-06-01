59d2126b5284f33e57ebdbd0828950ad974a6340
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2023-06-01 12:18:19 +0200

    ns-light set. added new icons

---
+++ faMissed.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fal";
+var iconName = "missed";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "M23.5607 5.93934C24.1464 6.52513 24.1464 7.47487 23.5607 8.06066L13.0607 18.5607C12.4849 19.1364 11.5551 19.1477 10.9655 18.5862L3 11V14.5C3 15.3284 2.32843 16 1.5 16C0.671573 16 0 15.3284 0 14.5V7.5C0 6.67157 0.671573 6 1.5 6H8C8.82843 6 9.5 6.67157 9.5 7.5C9.5 8.32843 8.82843 9 8 9H5.25L11.9744 15.4042L21.4393 5.93934C22.0251 5.35355 22.9749 5.35355 23.5607 5.93934Z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faMissed = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,14 @@ export const prefix: IconPrefix;
 export const fal: IconPack;
 
 export const faAddressBook: IconDefinition;
+export const faCameraSecurity: IconDefinition;
 export const faChartLine: IconDefinition;
 export const faClockRotateLeft: IconDefinition;
+export const faEarListen: IconDefinition;
+export const faGear: IconDefinition;
 export const faGrid2: IconDefinition;
 export const faHeadset: IconDefinition;
 export const faMessage: IconDefinition;
+export const faMissed: IconDefinition;
+export const faStar: IconDefinition;
 export const faUsers: IconDefinition;
