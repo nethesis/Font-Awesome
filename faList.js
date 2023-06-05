3306e09636b649138e16b0a053250acc756b5a2a
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2023-06-05 15:06:33 +0200

    ns-light set. added new icons

---
+++ faList.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fal";
+var iconName = "list";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "M1.5 2.25C1.08579 2.25 0.75 2.58579 0.75 3V6C0.75 6.41421 1.08579 6.75 1.5 6.75H4.5C4.91421 6.75 5.25 6.41421 5.25 6V3C5.25 2.58579 4.91421 2.25 4.5 2.25H1.5ZM2.25 5.25V3.75H3.75V5.25H2.25ZM1.5 9.75C1.08579 9.75 0.75 10.0858 0.75 10.5V13.5C0.75 13.9142 1.08579 14.25 1.5 14.25H4.5C4.91421 14.25 5.25 13.9142 5.25 13.5V10.5C5.25 10.0858 4.91421 9.75 4.5 9.75H1.5ZM2.25 12.75V11.25H3.75V12.75H2.25ZM0.75 18C0.75 17.5858 1.08579 17.25 1.5 17.25H4.5C4.91421 17.25 5.25 17.5858 5.25 18V21C5.25 21.4142 4.91421 21.75 4.5 21.75H1.5C1.08579 21.75 0.75 21.4142 0.75 21V18ZM2.25 18.75V20.25H3.75V18.75H2.25ZM8.5 3.75C8.08579 3.75 7.75 4.08579 7.75 4.5C7.75 4.91421 8.08579 5.25 8.5 5.25H23C23.4142 5.25 23.75 4.91421 23.75 4.5C23.75 4.08579 23.4142 3.75 23 3.75H8.5ZM7.75 12C7.75 11.5858 8.08579 11.25 8.5 11.25H23C23.4142 11.25 23.75 11.5858 23.75 12C23.75 12.4142 23.4142 12.75 23 12.75H8.5C8.08579 12.75 7.75 12.4142 7.75 12ZM8.5 18.75C8.08579 18.75 7.75 19.0858 7.75 19.5C7.75 19.9142 8.08579 20.25 8.5 20.25H23C23.4142 20.25 23.75 19.9142 23.75 19.5C23.75 19.0858 23.4142 18.75 23 18.75H8.5Z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faList = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
---
+++ faServer.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fal";
+var iconName = "server";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "M3 3H21C21.8284 3 22.5 3.67157 22.5 4.5V7.5C22.5 8.32843 21.8284 9 21 9H3C2.17157 9 1.5 8.32843 1.5 7.5V4.5C1.5 3.67157 2.17157 3 3 3ZM0 4.5C0 2.84315 1.34315 1.5 3 1.5H21C22.6569 1.5 24 2.84315 24 4.5V7.5C24 9.15685 22.6569 10.5 21 10.5H3C1.34315 10.5 0 9.15685 0 7.5V4.5ZM3 15H21C21.8284 15 22.5 15.6716 22.5 16.5V19.5C22.5 20.3284 21.8284 21 21 21H3C2.17157 21 1.5 20.3284 1.5 19.5V16.5C1.5 15.6716 2.17157 15 3 15ZM0 16.5C0 14.8431 1.34315 13.5 3 13.5H21C22.6569 13.5 24 14.8431 24 16.5V19.5C24 21.1569 22.6569 22.5 21 22.5H3C1.34315 22.5 0 21.1569 0 19.5V16.5ZM17 6C17 6.55228 16.5523 7 16 7C15.4477 7 15 6.55228 15 6C15 5.44772 15.4477 5 16 5C16.5523 5 17 5.44772 17 6ZM16 19C16.5523 19 17 18.5523 17 18C17 17.4477 16.5523 17 16 17C15.4477 17 15 17.4477 15 18C15 18.5523 15.4477 19 16 19ZM20.5 6C20.5 6.55228 20.0523 7 19.5 7C18.9477 7 18.5 6.55228 18.5 6C18.5 5.44772 18.9477 5 19.5 5C20.0523 5 20.5 5.44772 20.5 6ZM19.5 19C20.0523 19 20.5 18.5523 20.5 18C20.5 17.4477 20.0523 17 19.5 17C18.9477 17 18.5 17.4477 18.5 18C18.5 18.5523 18.9477 19 19.5 19Z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faServer = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
