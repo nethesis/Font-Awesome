600d7b4da04eb51ba133b8be1a6c0d34880b16b8
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2023-06-05 15:08:02 +0200

    ns-solid set. added new icons

---
+++ faEmptySet.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fas";
+var iconName = "empty-set";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "M0.439341 2.56066C-0.146446 1.97487 -0.146446 1.02513 0.439341 0.43934C1.02513 -0.146447 1.97487 -0.146447 2.56066 0.43934L5.71174 3.59042C7.46461 2.27761 9.6415 1.5 12 1.5C17.799 1.5 22.5 6.20101 22.5 12C22.5 14.3585 21.7224 16.5354 20.4096 18.2883L23.5607 21.4393C24.1464 22.0251 24.1464 22.9749 23.5607 23.5607C22.9749 24.1464 22.0251 24.1464 21.4393 23.5607L18.2883 20.4096C16.5354 21.7224 14.3585 22.5 12 22.5C6.20101 22.5 1.5 17.799 1.5 12C1.5 9.6415 2.27761 7.46461 3.59042 5.71174L0.439341 2.56066ZM5.7426 7.86391C4.95732 9.04956 4.5 10.4714 4.5 12C4.5 16.1421 7.85786 19.5 12 19.5C13.5286 19.5 14.9504 19.0427 16.1361 18.2574L5.7426 7.86391ZM18.2574 16.1361C19.0427 14.9504 19.5 13.5286 19.5 12C19.5 7.85786 16.1421 4.5 12 4.5C10.4714 4.5 9.04956 4.95732 7.86391 5.7426L18.2574 16.1361Z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faEmptySet = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
