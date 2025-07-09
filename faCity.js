68498993562d2c62b5361b1b9852ae48ed08f16c
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2025-07-09 10:55:40 +0200

    ns-light set. added new icons

---
+++ faCity.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fal";
+var iconName = "city";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "m4.5 16.5c0.6 0 1 0.4 1 1v1c0 0.6 -0.4 1 -1 1h-1c-0.6 0 -1 -0.4 -1 -1v-1c0 -0.6 0.4 -1 1 -1zm0 -4c0.6 0 1 0.4 1 1v1c0 0.6 -0.4 1 -1 1h-1c-0.6 0 -1 -0.4 -1 -1v-1c0 -0.6 0.4 -1 1 -1zm0 -4c0.6 0 1 0.4 1 1v1c0 0.6 -0.4 1 -1 1h-1c-0.6 0 -1 -0.4 -1 -1v-1c0 -0.6 0.4 -1 1 -1zm9.5 5.5c0.6 0 1 0.4 1 1v1c0 0.6 -0.4 1 -1 1h-1c-0.6 0 -1 -0.4 -1 -1v-1c0 -0.6 0.4 -1 1 -1zm0 -4c0.6 0 1 0.4 1 1v1c0 0.6 -0.4 1 -1 1h-1c-0.6 0 -1 -0.4 -1 -1v-1c0 -0.6 0.4 -1 1 -1zm0 -4c0.6 0 1 0.4 1 1v1c0 0.6 -0.4 1 -1 1h-1c-0.6 0 -1 -0.4 -1 -1v-1c0 -0.6 0.4 -1 1 -1zm2 -3c1.1 0 2 0.9 2 2v5h4h0.2c1 0.1 1.8 1 1.8 2v8v0.2c-0.1 0.9 -0.9 1.7 -1.8 1.8h-0.2h-20h-0.2c-0.9 -0.1 -1.7 -0.9 -1.8 -1.8v-0.2v-12c0 -1.1 0.9 -2 2 -2h0.5v-2.3c0 -0.4 0.3 -0.7 0.8 -0.7c0.4 0 0.7 0.3 0.7 0.7v2.3h2v-2.3c0 -0.4 0.3 -0.7 0.8 -0.7c0.4 0 0.7 0.3 0.7 0.7v2.3h1.5v-1c0 -1.1 0.9 -2 2 -2zm-14 4.5c-0.3 0 -0.5 0.2 -0.5 0.5v12c0 0.3 0.2 0.5 0.5 0.5h7v-13zm9 -3c-0.3 0 -0.5 0.2 -0.5 0.5v15.5h6v-15.5c0 -0.3 -0.2 -0.5 -0.5 -0.5zm7 16h4c0.3 0 0.5 -0.2 0.5 -0.5v-8c0 -0.3 -0.2 -0.5 -0.5 -0.5h-4z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faCity = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
---
+++ faUnitGroups.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fal";
+var iconName = "unit-groups";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "m15.3 14.1c0.5 0 0.9 0.4 0.9 0.9c0 0.5 -0.4 0.9 -0.9 0.9c-0.5 0 -0.8 -0.4 -0.8 -0.9c0 -0.5 0.3 -0.9 0.8 -0.9zm2.9 0c0.5 0 0.9 0.4 0.9 0.9c0 0.5 -0.4 0.9 -0.9 0.9c-0.5 0 -0.8 -0.4 -0.8 -0.9c0 -0.5 0.3 -0.9 0.8 -0.9zm1.3 -3.1c1.4 0 2.5 1.2 2.5 2.7v2.6c0 1.5 -1.1 2.7 -2.5 2.7h-15c-1.4 0 -2.5 -1.2 -2.5 -2.7v-2.6c0 -1.5 1.1 -2.7 2.5 -2.7zm-15 1.3c-0.7 0 -1.3 0.7 -1.3 1.4v2.6c0 0.7 0.6 1.4 1.3 1.4h15c0.7 0 1.2 -0.7 1.2 -1.4v-2.6c0 -0.7 -0.5 -1.4 -1.2 -1.4zm4.5 -7.3c1.1 0 2 1.1 2 2.4v1.2v0.2q0 0.2 -0.1 0.4h-1q0.1 -0.3 0.1 -0.6v-1.2c0 -0.7 -0.4 -1.2 -1 -1.2h-7c-0.6 0 -1 0.5 -1 1.2v1.2c0 0.7 0.4 1.2 1 1.2h0.9q-0.7 0.5 -1.2 1.2c-1 -0.2 -1.7 -1.2 -1.7 -2.4v-1.2c0 -1.3 0.8 -2.3 1.8 -2.4h0.2zm13.2 0c1 0.1 1.8 1.1 1.8 2.4v1.2c0 1.2 -0.7 2.2 -1.7 2.4q-0.5 -0.7 -1.2 -1.2h0.9c0.6 0 1 -0.5 1 -1.2v-1.2c0 -0.7 -0.4 -1.2 -1 -1.2h-7c-0.6 0 -1 0.5 -1 1.2v1.2q0 0.3 0.1 0.6h-1q-0.1 -0.2 -0.1 -0.4v-0.2v-1.2c0 -1.3 0.9 -2.4 2 -2.4h7z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faUnitGroups = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
