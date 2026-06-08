42a41a56dbee88805a15d3433672bdd952a1f505
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2026-06-08 09:18:01 +0200

    ns-light set. added new icons

---
+++ faTriangleExclamation.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fal";
+var iconName = "triangle-exclamation";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "m12 17c0.55 0 1 0.45 1 1c0 0.55 -0.45 1 -1 1c-0.55 0 -1 -0.45 -1 -1c0 -0.55 0.45 -1 1 -1zm0 -9.82c0.55 0 1 0.45 1 1v6.09c0 0.55 -0.45 1 -1 1c-0.55 0 -1 -0.45 -1 -1v-6.09c0 -0.55 0.45 -1 1 -1zm-2.33 -4.8c1.04 -1.84 3.62 -1.84 4.66 0l9.31 16.5c1.03 1.83 -0.26 4.12 -2.33 4.12h-18.62c-2.07 0 -3.36 -2.29 -2.33 -4.12zm3.39 0.75c-0.47 -0.84 -1.65 -0.84 -2.12 0l-9.3 16.5c-0.47 0.83 0.11 1.87 1.05 1.87h18.62c0.94 0 1.53 -1.04 1.06 -1.87z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faTriangleExclamation = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
