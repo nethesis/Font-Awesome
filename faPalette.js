581a497e2f8e45634e9aef3f5cfc199489cc455f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2026-08-28 13:06:13 +0200

    ns-light set. added new icons

---
+++ faPalette.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "fal";
+var iconName = "palette";
+var width = 24;
+var height = 24;
+var aliases = [];
+var unicode = "";
+var svgPathData = "m12.01 0c6.76 0 11.99 5 11.99 12.55c0 0.95 -1.47 2.45 -3 2.45h-5c-1 0 -1.52 0.27 -2 1c-0.54 0.81 -0.31 1.57 0 2.5c0.5 1.5 1 1.5 1 3c0 1.18 -1.32 2.5 -2.5 2.5c-6.99 0 -12.22 -4.87 -12.49 -11.5c-0.28 -6.76 5.23 -12.5 12 -12.5zm0 1.5c-5.91 0 -10.74 5.03 -10.5 10.94c0.24 5.77 4.75 10.06 10.99 10.06c0.06 0 0.33 -0.07 0.63 -0.37c0.3 -0.3 0.37 -0.57 0.37 -0.63c0 -0.52 -0.07 -0.67 -0.15 -0.83c-0.13 -0.25 -0.48 -0.81 -0.77 -1.7c-0.29 -0.84 -0.81 -2.31 0.17 -3.8c0.33 -0.5 0.76 -0.97 1.38 -1.28c0.6 -0.31 1.24 -0.39 1.87 -0.39h5c0.29 0 0.7 -0.16 1.08 -0.49q0.27 -0.25 0.38 -0.44q0.03 -0.06 0.04 -0.09c-0.03 -6.71 -4.61 -10.98 -10.49 -10.98zm5.24 5.5c0.69 0 1.25 0.56 1.25 1.25c0 0.69 -0.56 1.25 -1.25 1.25c-0.69 0 -1.25 -0.56 -1.25 -1.25c0 -0.69 0.56 -1.25 1.25 -1.25zm-5.49 -2.5c0.68 0 1.24 0.56 1.24 1.25c0 0.69 -0.56 1.25 -1.24 1.25c-0.69 0 -1.25 -0.56 -1.25 -1.25c0 -0.69 0.56 -1.25 1.25 -1.25zm-6.5 7.5c0.69 0 1.25 0.56 1.25 1.25c0 0.69 -0.56 1.25 -1.25 1.25c-0.69 0 -1.25 -0.56 -1.25 -1.25c0 -0.69 0.56 -1.25 1.25 -1.25zm1.5 -5c0.69 0 1.25 0.56 1.25 1.25c0 0.69 -0.56 1.25 -1.25 1.25c-0.69 0 -1.25 -0.56 -1.25 -1.25c0 -0.69 0.56 -1.25 1.25 -1.25z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faPalette = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
